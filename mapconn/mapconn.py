94289d21d029c43d9dfb91682b5666a623c2e23f
LeonDLotter <leondlotter@gmail.com>
2025-03-13 14:25:34 +0100

    lr_mirror_dist_mat default to False

--- mapconn/mapconn.py
+++ mapconn/mapconn.py
@@ -870,7 +870,7 @@ class MapConnNull:
         if map_data_null is None:
             null_kwargs = {
                 "method": "moran",
-                "lr_mirror_dist_mat": True,
+                "lr_mirror_dist_mat": False,
                 "lr_mirror_null_maps": False,
                 "parc_idc_lh": None,
                 "parc_idc_rh": None,
